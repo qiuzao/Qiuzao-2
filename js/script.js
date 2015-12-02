e9030815cc2a578bbd1013c85600f6d1d89a26ef
Xiao Ma <selkie@outlook.com>
2015-12-01 23:44:16 -0500

    true another retina detection

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(function() {
 
 	topEffect();
 
-	if (isRetina()) {
+	if (isRetinaDisplay()) {
 		$.each($(".works-image"), function(index, el) {
 			var imgsrc = $(el).attr("src");
 			var newsrc = imgsrc.slice(0, imgsrc.length - 4) + "@2x" + imgsrc.slice(imgsrc.length - 4);
@@ -25,4 +25,11 @@ $(function() {
 	function isRetina(){
 	    return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio >= 2)) && /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
 	}
+
+	function isRetinaDisplay() {
+        if (window.matchMedia) {
+            var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
+            return (mq && mq.matches || (window.devicePixelRatio > 1)); 
+        }
+    }
 });
