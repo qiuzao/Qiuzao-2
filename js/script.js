84f5687a73f2afc0bb5635b8734cc5173334b896
Xiao Ma <selkie@outlook.com>
2015-12-01 23:35:46 -0500

    retina detection

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ $(function() {
 
 	topEffect();
 
+	if (isRetina()) {
+		$.each($(".works-image"), function(index, el) {
+			// add @2x
+		});
+	};
+
 	function topEffect() {
 		$.each($(".top-decor"), function(index, el) {
 			window.setTimeout(function() {
@@ -12,5 +18,9 @@ $(function() {
 		});
 
 		$("#slogan-text").delay(700).fadeTo(700, 1);
+	}
+
+	function isRetina(){
+	    return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio >= 2)) && /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
 	}
 });
